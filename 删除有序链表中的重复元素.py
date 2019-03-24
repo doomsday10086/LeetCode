0ce141bfdf2a0f4ba98baaea40ab1cfe28de55a6
shen <18573151260@163.com>
2019-03-25 00:31:32 +0800

    simple singlelink

--- 删除有序链表中的重复元素.py
+++ 删除有序链表中的重复元素.py
@@ -25,3 +25,21 @@ class Solution:
                 cur = cur.next
 
         return head
+
+
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+# 示例:
+# 输入: [-2,1,-3,4,-1,2,1,-5,4],
+# 输出: 6
+# 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+class Solution:
+    def maxSubArray(self, nums):
+        sum = 0  # 阶段性和
+        max_sub_sum = nums[0]  # 最终结果
+        for num in nums:
+            sum += num
+            if sum > max_sub_sum:
+                max_sub_sum = sum
+            if sum < 0:
+                sum = 0
+        return max_sub_sum
